4b648967410a010908d5ffeadb9546826a560978
amm834 <aungmyatmoe834@gmail.com>
2021-06-09 01:08:35 +0630

    Fix:Conflicts

---
+++ app.js
@@ -0,0 +1,3 @@
+import Luhn from './lib/luhn.js';
+//var Luhn = require("./lib/luhn.js");
+console.log(Luhn.validate(123))
--- src/luhn.ts
+++ src/luhn.ts
@@ -1,24 +1,22 @@
-class Luhn {
-    /**
-    * @description Validatator of Credit Card Numbers
-    * @param pan Credit Card Numbers
-    * @returns Validation result of Credit Card Nunbers that are valid or not
-    */
-    public static validate(pan: any): boolean {
-        // Change to number object
-        const  cc = Array.from(String(pan), Number);
-        // Luhn Alogorithom
-        let sum: number = 0;
-        for (let i = 0; i <= cc.length - 1; i++) {
-            let digit: number = cc[i];
-            if (i % 2 === 0) {
-                digit *= 2;
-                if (digit > 9) digit -= 9;
-            }
-            sum += digit;
-        }
-        return sum % 10 === 0;
+module.exports = class Luhn {
+  /**
+  * @description Validatator of Credit Card Numbers
+  * @param cc Credit Card Numbers
+  * @returns Validation result of Credit Card Nunbers that are valid or not
+  */
+  public static validate(cc: any): boolean {
+    // Change to number object
+    cc = Array.from(String(cc), Number);
+    // Luhn Alogorithom
+    let sum: number = 0;
+    for (let i = 0; i <= cc.length - 1; i++) {
+      let digit: number = cc[i];
+      if (i % 2 === 0) {
+        digit *= 2;
+        if (digit > 9) digit -= 9;
+      }
+      sum += digit;
     }
+    return sum % 10 === 0;
+  }
 }
-
-console.log(Luhn.validate(124))
