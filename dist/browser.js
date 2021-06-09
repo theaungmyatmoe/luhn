f44bd4fb1f4f05c0449d6d1d1fbbb9a26415b632
amm834 <aungmyatmoe834@gmail.com>
2021-06-10 03:57:14 +0630

    Fix:Remove files

--- dist/browser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const luhn_1 = __importDefault(require("./luhn"));
-window.Luhn = luhn_1.default;
-//# sourceMappingURL=browser.js.map
